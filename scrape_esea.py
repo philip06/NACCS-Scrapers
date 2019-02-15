a9a3edf3d55cd24a0416be5664d1c81da75aefcc
EC2 Default User <ec2-user@ip-172-31-46-111.us-east-2.compute.internal>
2019-02-15 23:01:04 +0000

    Implemented OOP version of scoreboard scraper

--- scrape_esea.py
+++ scrape_esea.py
@@ -1,54 +1,76 @@
 import requests
 import re
-from bs4 import  BeautifulSoup as soup
+import json
+from bs4 import BeautifulSoup as soup
 from pprint import pprint
-
-user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.517 Safari/537.36'
-
-headers = {
-    "User-Agent": user_agent
-}
-
-
-def get_matches(req_url):
-    response = requests.get(req_url, headers=headers)
-    pprint(response.text)
-    page_soup = soup(response.text, "html.parser")
-    # uses class searching in bs4 to find and parse table
-    matches = page_soup.find("table", {"class": "club-recent-matches-table"})
-    match_ids = []
-
-    # regex filtering to remove html
-    for match in matches:
-        match_ids.append(re.findall("href=\"(.*?)\">", str(match))[0])
-
-    return matches
     
-# class Scoreboard:
-#     def __init__(self, monitor_url):
-
-def get_proxy(retry_count):
-    req_url = "http://pubproxy.com/api/proxy"
+# class used to pull in fresh data at given interval (seconds)
+class Scoreboard:
+    def __init__(self, match_url, refresh_rate=5):
+        self.match_url = match_url
+        self.refresh_rate = refresh_rate
         
-
-def get_scoreboards(req_url):
-    response = requests.get(req_url, headers=headers)
-    page_soup = soup(response.text, "html.parser")
-    page_soup = page_soup.find("div", {"id": "stats-match-view"})# {"class": "match-header"})
-    # list of all tables within div wrapper
-    table_list = page_soup.find_all("table")
-    # translate soup objects to unicode
-    table_list = [x.text for x in table_list]
+        user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.517 Safari/537.36'
+        self.headers = {
+            "User-Agent": user_agent
+        }
     
-    def parse_period_scores(inner_soup):
-        return inner_soup.find()
+    # method to create feed updated at set interval
+    # TODO
+    def get_data_feed(self):
+        print("get_data_feed")
+
+    # method to pull in a fresh proxy
+    # TODO
+    def get_proxy(self, retry_count=1):
+        req_url = "http://pubproxy.com/api/proxy"
+        while retry_count >= 0:
+            ret_body = requests.get(req_url).text
+            pprint(ret_body)
+            ret_json = json.loads(ret_body)
+            pprint(ret_json["ip"])
+            break
+    
+    def parse_html_table(self, table):
+        table_data = []
+        table_headers = []
         
-    # def parse_regex(reg, table):
-    #     re_sequence = re.compile(reg, re.MULTILINE)
+        for tr in table.find_all('tr')[1:]:
+            table_data.append([td.string.strip() for td in tr.find_all('td') if td.string and td.string.strip() != ""])
+            
+            table_headers.append([th.string.strip() for th in tr.find_all('th') if th.string and td.string.strip() != ""])
+            
+        pprint(table_data)
+        pprint(table_headers)
+        return table_data
+    
+    # method run on each refresh to collect new data
+    def get_scoreboards(self, proxies_dict):
+        response = requests.get(self.match_url, headers=self.headers, proxies=proxies_dict)
+        page_soup = soup(response.text, "html.parser")
+        page_soup = page_soup.find("div", {"id": "stats-match-view"})# {"class": "match-header"})
+        # list of all tables within div wrapper
+        table_list = page_soup.find_all("table")
         
-    for table in table_list:
+        # # translate soup objects to unicode
+        # table_list = [x.text for x in table_list]
         
+        period_table = []
+        
+        def parse_period_scores(inner_soup):
+            period_table = self.parse_html_table(inner_soup)
+            
+        for table in table_list:
+            if table.find(text=re.compile("Period Scores")):
+                parse_period_scores(table)
+                pprint(period_table)
+                break
 
 req_url = "https://play.esea.net/match/14570353"
-
-get_scoreboards(req_url)
+proxies_dict = {
+                    "http": "35.245.183.119:3128"
+                }
+        
+s = Scoreboard(req_url)
+s.get_scoreboards(proxies_dict)
+# proxy = get_proxy(retry_count=1)
