ef7d83d0f73c5847266d5a48594976576d48cf8f
EC2 Default User <ec2-user@ip-172-31-46-111.us-east-2.compute.internal>
2019-02-16 02:39:11 +0000

    Implemented ability to parse esea's tables into JSON data

--- scrape_esea.py
+++ scrape_esea.py
@@ -31,6 +31,7 @@ class Scoreboard:
             pprint(ret_json["ip"])
             break
     
+    # parses generic html table for header data and table data
     def parse_html_table(self, table):
         table_data = []
         table_headers = []
@@ -38,11 +39,13 @@ class Scoreboard:
         for tr in table.find_all('tr')[1:]:
             table_data.append([td.string.strip() for td in tr.find_all('td') if td.string and td.string.strip() != ""])
             
-            table_headers.append([th.string.strip() for th in tr.find_all('th') if th.string and td.string.strip() != ""])
+            table_headers.append([th.string.strip() for th in tr.find_all('th') if th.string and th.string.strip() != ""])
             
-        pprint(table_data)
-        pprint(table_headers)
-        return table_data
+        # filter out empty lists
+        table_data = [data for data in table_data if data]
+        table_headers = [data for data in table_headers if data]
+
+        return table_headers, table_data
     
     # method run on each refresh to collect new data
     def get_scoreboards(self, proxies_dict):
@@ -55,16 +58,42 @@ class Scoreboard:
         # # translate soup objects to unicode
         # table_list = [x.text for x in table_list]
         
-        period_table = []
+        ret_json = {}
         
-        def parse_period_scores(inner_soup):
-            period_table = self.parse_html_table(inner_soup)
-            
+        period_headers, period_body = [], []
+        stats_headers, stats_body = [], []
+        
+        # filter html tables by type
         for table in table_list:
+            # filter for data from period table
             if table.find(text=re.compile("Period Scores")):
-                parse_period_scores(table)
-                pprint(period_table)
-                break
+                period_headers, period_body = self.parse_html_table(table)
+            
+            # filter for data from stats tables
+            elif table.find(text=re.compile("Statistics")):
+                stats_headers, temp_body = self.parse_html_table(table)
+                stats_body.append(temp_body)
+        
+        # pprint(period_body)
+        # pprint(stats_headers)
+        
+        team_captains = []
+        period_headers_filtered = []
+        
+        # pull out team captain names from period headers
+        for h in period_headers:
+            if len(h) == 1:
+                team_captains.append(h[0])
+            else:
+                period_headers_filtered = h
+                
+        # generate data structure to be converted into json
+        for i, name in enumerate(team_captains):
+            ret_json["stats_table_team"+str(i)] = {"captain": name, "headers": stats_headers[0], "body": stats_body[i]}
+            # add period table data
+            ret_json["period_table"] = {"headers": period_headers_filtered, "body": period_body}
+        
+        pprint(json.dumps(ret_json))
 
 req_url = "https://play.esea.net/match/14570353"
 proxies_dict = {
