0ca3f1105d8c8f5ea7174bb3f96ad72880f6ff3d
EC2 Default User <ec2-user@ip-172-31-46-111.us-east-2.compute.internal>
2019-02-16 04:23:00 +0000

    Implemented error handling

--- scrape_esea.py
+++ scrape_esea.py
@@ -1,5 +1,6 @@
 import requests
 import re
+import logging
 import json
 from bs4 import BeautifulSoup as soup
 from pprint import pprint
@@ -7,6 +8,9 @@ from pprint import pprint
 # class used to pull in fresh data at given interval (seconds)
 class Scoreboard:
     def __init__(self, match_url, refresh_rate=5):
+        self.scoreboard_log = logging.getLogger("scoreboard_log")
+        logging.basicConfig(filename='scoreboard.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s')
+        
         self.match_url = match_url
         self.refresh_rate = refresh_rate
         
@@ -36,66 +40,72 @@ class Scoreboard:
         table_data = []
         table_headers = []
         
-        for tr in table.find_all('tr')[1:]:
-            table_data.append([td.string.strip() for td in tr.find_all('td') if td.string and td.string.strip() != ""])
-            
-            table_headers.append([th.string.strip() for th in tr.find_all('th') if th.string and th.string.strip() != ""])
-            
-        # filter out empty lists
-        table_data = [data for data in table_data if data]
-        table_headers = [data for data in table_headers if data]
-
-        return table_headers, table_data
+        try:
+            # filter table rows and headers into lists
+            for tr in table.find_all('tr')[1:]:
+                table_data.append([td.string.strip() for td in tr.find_all('td') if td.string and td.string.strip() != ""])
+                
+                table_headers.append([th.string.strip() for th in tr.find_all('th') if th.string and th.string.strip() != ""])
+                
+            # filter out empty lists
+            table_data = [data for data in table_data if data]
+            table_headers = [data for data in table_headers if data]
+    
+            return table_headers, table_data
+        except Exception as e:
+            self.scoreboard_log.error(e)
+            return "Error"
     
     # method run on each refresh to collect new data
+    # TODO: add proper error handling
     def get_scoreboards(self, proxies_dict):
-        response = requests.get(self.match_url, headers=self.headers, proxies=proxies_dict)
-        page_soup = soup(response.text, "html.parser")
-        page_soup = page_soup.find("div", {"id": "stats-match-view"})# {"class": "match-header"})
-        # list of all tables within div wrapper
-        table_list = page_soup.find_all("table")
-        
-        # # translate soup objects to unicode
-        # table_list = [x.text for x in table_list]
-        
-        ret_json = {}
-        
-        period_headers, period_body = [], []
-        stats_headers, stats_body = [], []
-        
-        # filter html tables by type
-        for table in table_list:
-            # filter for data from period table
-            if table.find(text=re.compile("Period Scores")):
-                period_headers, period_body = self.parse_html_table(table)
+        try:
+            response = requests.get(self.match_url, headers=self.headers, proxies=proxies_dict)
+            page_soup = soup(response.text, "html.parser")
+            page_soup = page_soup.find("div", {"id": "stats-match-view"})
             
-            # filter for data from stats tables
-            elif table.find(text=re.compile("Statistics")):
-                stats_headers, temp_body = self.parse_html_table(table)
-                stats_body.append(temp_body)
-        
-        # pprint(period_body)
-        # pprint(stats_headers)
-        
-        team_captains = []
-        period_headers_filtered = []
-        
-        # pull out team captain names from period headers
-        for h in period_headers:
-            if len(h) == 1:
-                team_captains.append(h[0])
-            else:
-                period_headers_filtered = h
+            # list of all tables within div wrapper
+            table_list = page_soup.find_all("table")
+            
+            ret_json = {}
+            
+            period_headers, period_body = [], []
+            stats_headers, stats_body = [], []
+            
+            # filter html tables by type
+            for table in table_list:
+                # filter for data from period table
+                if table.find(text=re.compile("Period Scores")):
+                    period_headers, period_body = self.parse_html_table(table)
                 
-        # generate data structure to be converted into json
-        for i, name in enumerate(team_captains):
-            ret_json["stats_table_team"+str(i)] = {"captain": name, "headers": stats_headers[0], "body": stats_body[i]}
-            # add period table data
-            ret_json["period_table"] = {"headers": period_headers_filtered, "body": period_body}
-        
-        pprint(json.dumps(ret_json))
-
+                # filter for data from stats tables
+                elif table.find(text=re.compile("Statistics")):
+                    stats_headers, temp_body = self.parse_html_table(table)
+                    stats_body.append(temp_body)
+            
+            team_captains = []
+            period_headers_filtered = []
+            
+            # pull out team captain names from period headers
+            for h in period_headers:
+                if len(h) == 1:
+                    team_captains.append(h[0])
+                else:
+                    period_headers_filtered = h
+                    
+            # generate data structure to be converted into json
+            for i, name in enumerate(team_captains):
+                ret_json["stats_table_team"+str(i)] = {"captain": name, "headers": stats_headers[0], "body": stats_body[i]}
+                # add period table data
+                ret_json["period_table_team"+str(i)] = {"captain": name, "headers": period_headers_filtered, "body": period_body[i]}
+            
+            return json.dumps(ret_json)
+        except Exception as e:
+            self.scoreboard_log.error(e)
+            return "Error"
+            
 req_url = "https://play.esea.net/match/14570353"
+
 proxies_dict = {
                     "http": "35.245.183.119:3128"
                 }
